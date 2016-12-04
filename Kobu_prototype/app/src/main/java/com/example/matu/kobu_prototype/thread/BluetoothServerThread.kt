ad3f625dbeda103b8ffa0aac1c7f09eae13287a5
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2016-12-04 17:31:58 +0900

    Bluetooth用のserverとclientのthreadを作成

---
+++ Kobu_prototype/app/src/main/java/com/example/matu/kobu_prototype/thread/BluetoothServerThread.kt
@@ -0,0 +1,74 @@
+package com.example.matu.kobu_prototype.thread
+
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import java.io.IOException
+import java.util.*
+
+
+/**
+ * Created by riku on 16/12/04.
+ */
+class BluetoothServerThread(mContext: Context, btAdapter: BluetoothAdapter) : Thread() {
+	//サーバー側の処理
+	//UUID：Bluetoothプロファイル毎に決められた値
+	private val PROTOTYPE_BLUETOOTH_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+	private val serveSock: BluetoothServerSocket?
+	private val myServerAdapter: BluetoothAdapter
+	private val mContext: Context
+
+
+	init {
+
+		var tmpServeSock: BluetoothServerSocket? = null
+		myServerAdapter = btAdapter
+		try {
+			//自デバイスのBluetoothサーバーソケットの取得
+			tmpServeSock = myServerAdapter.listenUsingRfcommWithServiceRecord("BlueToothSample03", PROTOTYPE_BLUETOOTH_UUID)
+		} catch (e: IOException) {
+			e.printStackTrace()
+		}
+
+		serveSock = tmpServeSock
+		this.mContext = mContext
+	}
+
+	override fun run() {
+		var receivedSocket: BluetoothSocket?
+		while (true) {
+			try {
+				//クライアント側からの接続要求待ち。ソケットが返される。
+				receivedSocket = serveSock?.accept()
+			} catch (e: IOException) {
+				break
+			}
+
+			if (receivedSocket != null) {
+				//ソケットを受け取れていた(接続完了時)の処理
+				//RwClassにmanageSocketを移す
+//				val rw = ReadWriteModel(mContext, receivedSocket, myNumber)
+//				rw.start()
+
+				try {
+					//処理が完了したソケットは閉じる。
+					serveSock?.close()
+				} catch (e: IOException) {
+					e.printStackTrace()
+				}
+
+				break
+			}
+		}
+	}
+
+	fun cancel() {
+		try {
+			serveSock?.close()
+		} catch (e: IOException) {
+		}
+
+	}
+}
